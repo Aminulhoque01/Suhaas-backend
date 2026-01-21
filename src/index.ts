aa43221fda32b6a99766937892e6b9d247ad9703
Aminulhoque01 <aminuhoque53@gmail.com>
2026-01-21 22:31:15 +0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import { connectDB } from "./config/bd.js";
+import app from "./app.js";
+dotenv.config();
+
+ 
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Server failed to start", error);
+    process.exit(1);
+  }
+};
+
+startServer();
