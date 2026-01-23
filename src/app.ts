b2de3a74fdfed395fb8460b022888365d4c64257
Aminulhoque01 <aminuhoque53@gmail.com>
2026-01-23 22:41:10 +0600

    ok

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,25 @@
 import express from "express";
 import cors from "cors";
+
 import userRoutes from "./modules/user/user.route";
 import authRoutes from "./modules/auth/auth.routes";
 import projectRoutes from "./modules/projects/project.route";
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 app.get("/health", (req, res) => {
-    res.status(200).json({ status: "OK" });
+  res.status(200).json({ status: "OK" });
 });
+
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/project", projectRoutes);
+
 app.get("/", (req, res) => {
-    res.json({ message: "API is working 🚀" });
+  res.json({ message: "API is working 🚀" });
 });
+
 export default app;
